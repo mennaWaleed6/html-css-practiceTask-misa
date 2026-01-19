94a77017e4b8375848fd1ae28c8432c8898b8a3e
mennaWaleed6 <mennaTallahMuhammad6@gmail.com>
2026-01-19 14:39:32 +0200

    Initial HTML & CSS practice task

---
+++ main.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.slider-content', {
+  rtl: true,
+  loop: true,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0:{
+        slidesPerView: 1
+    },
+    620:{
+        slidesPerView: 2
+    },
+    1024:{
+        slidesPerView: 3
+    },
+  }
+
+});
